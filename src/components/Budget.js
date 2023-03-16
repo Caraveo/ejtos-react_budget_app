bbff7052433efc8fdf586d1a34d2119271cd1e85
Caraveo <caraveo@me.com>
2023-03-16 00:28:08 -0600

    Added useState, and UseEffect

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses } = useContext(AppContext);
+    const budgetLimit = 20000;
+
+    const [budgetState, setBudgetState] = useState(budgetLimit);
+    //check the budgetState value is within the budgetLimit
+
+    useEffect(() => {
+            if(budgetState > budgetLimit)
+            {
+                console.log('Budget cannot be greater than 20000');
+                alert('The budget cannot be greater than 20000');
+                setBudgetState(budget);
+            }
+            if(budgetState > expenses)
+            {
+                console.log('You cannot reduce the budget value lower than expenses');
+                alert('You cannot reduce the budget value lower than expenses');
+                setBudgetState(budget);
+            }
+        }
+    );
+
     return (
+        <form> 
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £<input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budgetState}
+                    style={{ size: 10}}
+                    onChange={budgetState => setBudgetState(budgetState.target.value)}> 
+                </input></span>
         </div>
+        </form>
     );
 };
 export default Budget;
