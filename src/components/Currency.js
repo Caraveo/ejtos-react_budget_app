2dc4f4f7de8e512a3c3b8a0c8f3a943ec55c92f1
Caraveo <caraveo@me.com>
2023-03-16 11:15:11 -0600

    Currency selector

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,20 +2,21 @@ import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-    const { currency } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
 
-        const [currencyState, setCurrencyState] = useState(currency);
-
-        useEffect(() => {
-            
-        });
+        const changeCurrency = (val)=>{
+            dispatch({
+                type: 'CHG_CURRENCY',
+                payload: val,
+            })  
+          }
 
         return (
             <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
                 <div className="input-group-prepend">
                     <label className="input-group-text" htmlFor="inputGroupSelect01">Currency Type</label>
                 </div>
-                <select className="custom-select" id="inputGroupSelect01" onChange={(currency) => setCurrencyState(currency.target.value)}>
+                <select className="custom-select" id="inputGroupSelect01" onChange={event => changeCurrency(event.target.value)}>
                     <option defaultValue value="$" name="Dollar">$ Dollar</option>
                     <option value="£" name="Pound">£ Pound</option>
                     <option value="€" name="Euro">€ Euro</option>
